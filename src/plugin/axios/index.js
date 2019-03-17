7e772563274a8899cfbb4824e769bdfc4a9fe305
garychk <garychk@163.com>
2019-03-17 08:04:20 +0800

    update response filter

--- src/plugin/axios/index.js
+++ src/plugin/axios/index.js
@@ -66,6 +66,7 @@ service.interceptors.response.use(
     const dataAxios = response.data
     // 这个状态码是和后端约定的
     const { code } = dataAxios
+    
     // 根据 code 进行判断
     if (code === undefined) {
       // 如果没有 code 代表这不是项目后端开发的接口 比如可能是 D2Admin 请求最新版本
@@ -90,18 +91,18 @@ service.interceptors.response.use(
   error => {
     if (error && error.response) {
       switch (error.response.status) {
-        case 400: error.message = '请求错误'; break
+        case 400: error.message = error.response.data.error.details; break
         case 401: error.message = '未授权，请登录'; break
         case 403: error.message = '拒绝访问'; break
         case 404: error.message = `请求地址出错: ${error.response.config.url}`; break
         case 408: error.message = '请求超时'; break
-        case 500: error.message = '服务器内部错误'; break
+        case 500: error.message = error.response.data.error.details; break
         case 501: error.message = '服务未实现'; break
         case 502: error.message = '网关错误'; break
         case 503: error.message = '服务不可用'; break
         case 504: error.message = '网关超时'; break
         case 505: error.message = 'HTTP版本不受支持'; break
-        default: break
+        default: error.message = error.response.data.error.details; break
       }
     }
     errorLog(error)
