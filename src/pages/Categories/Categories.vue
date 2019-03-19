a8a855284560606c9e0e337312f0516818b8d306
garychk <garychk@163.com>
2019-03-19 22:50:01 +0800

    19-3-19

--- src/pages/Categories/Categories.vue
+++ src/pages/Categories/Categories.vue
@@ -1,7 +1,8 @@
 <template>
   <!-- d2-container 文档 https://doc.d2admin.fairyever.com/zh/sys-components/container.html -->
   <d2-container class="page-Categories">
-    <template slot="header">Categories</template>
+    <template slot="header">分类管理</template>
+    <el-tree :props="props" :load="loadNode" lazy show-checkbox @check-change="handleCheckChange"></el-tree>
     <template slot="footer">自动生成 0 个组件</template>
   </d2-container>
 </template>
@@ -10,33 +11,78 @@
 // 组件
 // 请删除下面代码中你不需要的部分
 export default {
-  name: 'Categories',
-  components: {
-  },
+  name: "Categories",
+  components: {},
   // 数据
-  data () {
-    return {}
+  data() {
+    return {
+      props: {
+        label: "name",
+        children: "zones"
+      },
+      count: 1
+    };
   },
   // 计算属性
   computed: {},
   // 侦听器
   watch: {},
   // 生命周期钩子
-  beforeCreate () {},
-  created () {},
-  beforeMount () {},
-  mounted () {},
-  beforeUpdate () {},
-  updated () {},
-  activated () {},
-  deactivated () {},
-  beforeDestroy () {},
-  destroyed () {},
+  beforeCreate() {},
+  created() {},
+  beforeMount() {},
+  mounted() {},
+  beforeUpdate() {},
+  updated() {},
+  activated() {},
+  deactivated() {},
+  beforeDestroy() {},
+  destroyed() {},
   // 事件处理方法
-  methods: {}
-}
+  methods: {
+    handleCheckChange(data, checked, indeterminate) {
+      console.log(data, checked, indeterminate);
+    },
+    handleNodeClick(data) {
+      console.log(data);
+    },
+    loadNode(node, resolve) {
+      if (node.level === 0) {
+        return resolve([{ name: "region1" }, { name: "region2" }]);
+      }
+      if (node.level > 3) return resolve([]);
+
+      var hasChild;
+      if (node.data.name === "region1") {
+        hasChild = true;
+      } else if (node.data.name === "region2") {
+        hasChild = false;
+      } else {
+        hasChild = Math.random() > 0.5;
+      }
+
+      setTimeout(() => {
+        var data;
+        if (hasChild) {
+          data = [
+            {
+              name: "zone" + this.count++
+            },
+            {
+              name: "zone" + this.count++
+            }
+          ];
+        } else {
+          data = [];
+        }
+
+        resolve(data);
+      }, 500);
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-@import './Categories.scss';
+@import "./Categories.scss";
 </style>
